1cd54f044b789c9d60cae851ef2df3f353f1697d
Jyoti Singh <jyo.singh88@gmail.com>
2023-05-12 19:59:17 -0400

    about section drafting

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,15 +1,24 @@
 import { Link } from "react-router-dom";
 import  "../Style/NavBar.css"
+import "./About.js"
 
 
 export default function NavBar() {
   return (
     <nav>
-      <Link to="/snacks"><h1><span>Snacks</span></h1></Link>
-      <br/>
+      <Link to="/snacks">
+        <h1>
+          <span>Snacks</span>
+        </h1>
+      </Link>
+      <br />
       <button>
-        <Link  to="/snacks/new">New Snack</Link>
+        <Link to="/snacks/new">New Snack</Link>
       </button>
+      <br />
+      <button>
+        <Link to="">About</Link>
+      </button> 
     </nav>
   );
 }
