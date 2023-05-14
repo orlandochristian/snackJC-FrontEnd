3ef68083c7ea2089c1786ed846e501aa1a7ad64e
Jyoti Singh <jyo.singh88@gmail.com>
2023-05-13 21:22:42 -0400

    modifications to nav,home,details

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,18 +3,8 @@ import Welcome from "../Components/Welcome.js"
 
 function Home() {
     return (
-<<<<<<< HEAD
-      <div className="home-container">
-        <h3 className="home-title">Welcome to the Snack App!</h3>
-        <img
-          src="https://www.yummytoddlerfood.com/wp-content/uploads/2021/06/after-school-snacks-on-countertop.jpg"
-          alt="Snack Art"
-          className="home-image"
-        />
-=======
       <div className="Snacks-App">
        < Welcome />
->>>>>>> cf581ccfea1e243a13deb94b10516ac51e53d049
       </div>
       //  <><div className="Snacks-App" style={{ textAlign: 'center' }}></div><div className="Snacks-App">
       //     <h3> Welcome to the Snacks App!</h3>
