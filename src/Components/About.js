99bed68487c651908c1dea22c23325dc7324d393
christian orlando <christianorlando@pursuit.org>
2023-05-14 21:02:10 -0400

    about v4

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,27 +6,24 @@ import '../Style/About.css';
 
 const About = () => {
   return (
-    <div>
-    <h3>Meet The Devs!</h3> 
+    <div >
+    <h3 className='mtd'>Meet The Devs!</h3> 
     <div className='team-about'>
+      <div className='bothjc'>
       <img
         className="team-photo"
         src="https://avatars.githubusercontent.com/u/115159441?v=4"
         alt="Christian Orlando"
       ></img>
-      <br></br>
+      
       <h4 className="bio-name">
         <em>Christian Orlando</em>
       </h4>
       <p className="bio-text-co">
-     
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum
+      I'm a full stack developer with a background in criminal justice from Queens,NY.
+       {/* {describe project focus} */}
+       My favorite snacks are chocolate covered pretzels and onigiri. 
+       Please check out my links below to connect with me.
       </p>
       <a href="https://github.com/orlandochristian">
         <img
@@ -44,14 +41,14 @@ const About = () => {
           src="./icons/linkedin.png"
         />
       </a>
-      <br></br>
-      <br></br>
+      </div>
+      <div className='bothjc'>
       <img
         className="team-photo"
         src="https://avatars.githubusercontent.com/u/47873538?v=4"
         alt="Jyoti Singh"
       ></img>
-      <br></br>
+      
       <h4 className="bio-name">
         <em>Jyoti Singh</em>
       </h4>
@@ -77,6 +74,7 @@ const About = () => {
           src="./icons/linkedin.png"
         />
       </a>
+    </div>
     </div>
     </div>
   );
